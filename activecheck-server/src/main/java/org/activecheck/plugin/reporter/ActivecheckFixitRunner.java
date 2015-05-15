c327e35c4730fc27fb178c794b62a65840dccc0c
Frederik Happel <mail@frederikhappel.de>
2015-05-15 16:43:29 +0200

    fixed bug that does not remove reporters from fixit queue bumped version to 1.1.2

--- activecheck-server/src/main/java/org/activecheck/plugin/reporter/ActivecheckFixitRunner.java
+++ activecheck-server/src/main/java/org/activecheck/plugin/reporter/ActivecheckFixitRunner.java
@@ -14,6 +14,7 @@ public class ActivecheckFixitRunner implements Runnable {
 			.getLogger(ActivecheckFixitRunner.class);
 
 	private Queue<ActivecheckReporter> fixitQueue = new ConcurrentLinkedQueue<ActivecheckReporter>();
+	private final int sleepMillis = 2000;
 
 	public void add(ActivecheckReporter reporter) {
 		fixitQueue.add(reporter);
@@ -22,14 +23,21 @@ public class ActivecheckFixitRunner implements Runnable {
 	@Override
 	public void run() {
 		while (true) {
-			for (ActivecheckReporter reporter : fixitQueue) {
+			ActivecheckReporter reporter = fixitQueue.poll();
+			while (reporter != null) {
 				if (reporter.getStatus() == ActivecheckReporterStatus.REQUEUE
 						|| reporter.getStatus() == ActivecheckReporterStatus.SCHEDULED) {
+					logger.info("FIXIT Running script for"
+							+ reporter.getOverallServiceName());
 					reporter.fixit();
+					logger.info("FIXIT Finished script for"
+							+ reporter.getOverallServiceName());
 				}
 			}
 			try {
-				TimeUnit.MILLISECONDS.sleep(2000);
+				logger.debug("FIXIT no scripts to run. Sleeping " + sleepMillis
+						+ " millis");
+				TimeUnit.MILLISECONDS.sleep(sleepMillis);
 			} catch (InterruptedException e) {
 				logger.error(e.getMessage());
 				logger.trace(e.getMessage(), e);
